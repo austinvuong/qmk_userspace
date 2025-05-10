7ab94b0eff10951f7d1f8fd2d34d47b06aa8671a
Austin Vuong <29992773+austinvuong@users.noreply.github.com>
2025-05-10 15:17:38 -0400

    Add colmaq-based overhaul, home row mods, and changed thumbs

---
+++ keyboards/redox/keymaps/austinvuong/config.h
@@ -0,0 +1,35 @@
+/*
+Copyright 2018 Mattia Dal Ben <matthewdibi@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define TAPPING_TOGGLE 3
+#define ONESHOT_TAP_TOGGLE 99
+#define ONESHOT_TIMEOUT 500
+#define TAPPING_TERM 300
+
+#define MOUSEKEY_INERTIA
+#define MOUSEKEY_DELAY 20
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_MAX_SPEED 22
+#define MOUSEKEY_TIME_TO_MAX 16
+#define MOUSEKEY_FRICTION 0.1
+#define MOUSEKEY_MOVE_DELTA 1
+
+// #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+// #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
---
+++ keyboards/redox/keymaps/austinvuong/rules.mk
@@ -0,0 +1,8 @@
+DEFAULT_FOLDER = redox/rev1/base
+
+# Disable unsupported hardware
+AUDIO_SUPPORTED = no
+BACKLIGHT_SUPPORTED = no
+
+MOUSEKEY_ENABLE = yes
+CAPS_WORDS_ENABLE = yes
